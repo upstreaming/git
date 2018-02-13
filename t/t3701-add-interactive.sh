4c3babe54daaa770cf8b4b30f1404ac0fc1f9d1b
Phillip Wood <phillip.wood@dunelm.org.uk>
2018-02-13 10:44:06 +0000

    t3701: add failing test for pathological context lines
    When a hunk is skipped by add -i the offsets of subsequent hunks are
    not adjusted to account for any missing insertions due to the skipped
    hunk. Most of the time this does not matter as apply uses the context
    lines to apply the subsequent hunks in the correct place, however in
    pathological cases the context lines will match at the now incorrect
    offset and the hunk will be applied in the wrong place. The offsets of
    hunks following an edited hunk that has had the number of insertions
    or deletions changed also need to be updated in the same way. Add
    failing tests to demonstrate this.
    
    Signed-off-by: Phillip Wood <phillip.wood@dunelm.org.uk>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3701-add-interactive.sh
+++ t/t3701-add-interactive.sh
@@ -493,4 +493,34 @@ test_expect_success 'add -p works even with color.ui=always' '
 	test_cmp expect actual
 '
 
+test_expect_success 'set up pathological context' '
+	git reset --hard &&
+	test_write_lines a a a a a a a a a a a >a &&
+	git add a &&
+	git commit -m a &&
+	test_write_lines c b a a a a a a a b a a a a >a &&
+	test_write_lines     a a a a a a a b a a a a >expected-1 &&
+	test_write_lines   b a a a a a a a b a a a a >expected-2 &&
+	write_script editor <<-\EOF
+	sed /^+c/d "$1" >"$1.tmp"
+	mv "$1.tmp" "$1"
+	EOF
+'
+
+test_expect_failure 'add -p works with pathological context lines' '
+	git reset &&
+	printf "%s\n" n y |
+	git add -p &&
+	git cat-file blob :a > actual &&
+	test_cmp expected-1 actual
+'
+
+test_expect_failure 'add -p patch editing works with pathological context lines' '
+	git reset &&
+	printf "%s\n" e y |
+	GIT_EDITOR=./editor git add -p &&
+	git cat-file blob :a > actual &&
+	test_cmp expected-2 actual
+'
+
 test_done
